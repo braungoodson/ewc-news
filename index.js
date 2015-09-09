013e6f5ef66a48052fc8e486b638eaefbaeccb55
Braun Goodson <braun.goodson@digitaslbi.com>
2015-09-09 17:28:55 -0400

    upgraded to use q

--- index.js
+++ index.js
@@ -1,88 +1,44 @@
+var Q = require('q');
+var Request = require('request');
+
 var News = {
-  news: function news(config) {
-    //console.log('executing news for ', config);
-    var keywords = config.keywords;
-    var done = config.done;
-    var queries = keywords.split(', ');
-    //console.log('using ' + queries.length + ' keywords');
-    var news = [];
-
-    for (var i = queries.length - 1; i >= 0; i--) {
-      //console.log(i + ' calling request with ' + queries[i]);
-      forEachRequest(i, queries[i], done);
-    }
-
-    function forEachRequest(i, q, done) {
-      request(q, function(response){
-        //console.log('begin response processing');
-        var object = JSON.parse(response);
-        var results = object.responseData.results;
-        for (var r in results) {
-          //console.log('got new result');
-          news.push(results[r]);
-        }
-        //console.log(i);
-        if (!i) {
-          //console.log('done');
-          done(news);
-        }
-      });
-    }
-
-    function request(query, callback) {
-
-      var http = require('http');
-
-      //console.log('making request for ' + query)
-
-      var options = {
-        hostname: 'ajax.googleapis.com',
-        port: 80,
-        path: '/ajax/services/search/news?v=1.0&q='+escape(query),
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Referer': 'http://gdsn.io'
-        }
-      };
-
-      //console.log('set options');
-
-      var req;
-      //console.log('calling http.request');
-      req = http.request(options, requestHandler);
-      //console.log('binding error handler');
-      req.on('error', errorHandler);
-      //console.log('ending request');
-      req.end();
-
-      function requestHandler(res) {
-        //console.log('begining requestHandler');
-        res.setEncoding('utf8');
-        var chunks = '';
-        res.on('data', function (chunk) {
-          //console.log('got data');
-          chunks += chunk;
-        });
-        res.on('end', function () {
-          //console.log('end data');
-          callback(chunks);
-        });
-      }
-
-      function errorHandler(e) {
-        //console.log(e.message);
-        throw new Error(e.message);
-      }
-
-      function escape(q) {
-        //console.log('escaping ' + q);
-        var escaped = q.replace(/ /g, '%20');
-        //console.log('escaped ' + escaped);
-        return escaped;
+  news: function (queries) {
+    var requests = [];
+    var keyword;
+    for (var q in queries) {
+      keyword = escape(queries[q].keyword);
+      for (var i = 0; i < queries[q].pages; i++) {
+        requests.push(request(resource(keyword,(i*4)+queries[q].start)));
       }
     }
+    return Q.all(requests);
   }
 };
+
+function escape(string) {
+  var escaped = string.replace(/ /g, '%20');
+  return escaped;
+}
+
+function resource(query, page) {
+  var protocol = 'http://';
+  var hostname = 'ajax.googleapis.com';
+  var path = '/ajax/services/search/news?v=1.0&q=';
+  var start = '&start='+page;
+  var resource = protocol + hostname + path + query + start;
+  return resource;
+}
+
+function request(resource) {
+  var d = Q.defer();
+  Request(resource, function (error, response, body) {
+    if (error) {
+      d.reject(error);
+    } else {
+      d.resolve(body);
+    }
+  });
+  return d.promise;
+}
 
 module.exports = News;
--- test/index.js
+++ test/index.js
@@ -1,8 +1,23 @@
 var News = require('../index.js');
 
-News.news({
-	keywords: 'large hadron collider, half-life, metallica',
-	done: function(news) {
-		console.log(news);
+News.news([{
+	keyword: 'ceres',
+	pages: 3,
+	start: 0
+}, {
+	keyword: 'pluto',
+	pages: 3,
+	start: 0
+}]).then(function(responses){
+	var news = [];
+	for (var r in responses) {
+		var response = JSON.parse(responses[r]);
+		var results = response.responseData.results;
+		for (var r in results) {
+			console.log(results[r].title);
+			news.push(results[r]);
+		}
 	}
+}).fail(function(error){
+	console.log(error);
 });
